2a90c8c3a4c3d094a51a0c0615580347c6d3d46a
santimontironi <santiimontironi@gmail.com>
2025-06-14 12:16:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.text())
+app.use(express.json())
+
+app.post('/user',(req,res) => {
+    console.log(req.body)
+    res.send("enviando un usuario")
+})
+
+app.get('/persons/:person',(req,res) => {
+    console.log(req.params.person)
+    res.send(`Hola ${req.params.person}`)
+})
+
+app.get('/numbers/:x/:y',(req,res) => {
+    const num1 = parseInt(req.params.x)
+    const num2 = parseInt(req.params.y)
+
+    const suma = num1 + num2
++
+    console.log(suma)
+
+    console.log(req.query.pepe)
+
+    res.send(`El resultado de la suma es ${suma}`)
+})
+
+//middleWare
+app.use((req,res,next) => {
+    console.log("pasa por acá")
+    next()
+})
+
+app.get('/profile',(req,res) => {
+    res.send("Profile")
+})
+
+app.listen(3000)
+
+console.log("Corriendo en el puerto 3000")
